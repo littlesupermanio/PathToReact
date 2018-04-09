85df3d940dcfc54a0c8434e90bba9432c3395375
DylanHo <hhb_1997@outlook.com>
2018-04-09 12:09:42 +0800

    Second commit

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyCYmJHZnWie9nkl-4myCixEnrC5NXwubTE';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos }); 
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
