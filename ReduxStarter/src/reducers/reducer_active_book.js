6cc2a97d3c7340f4aa2036571303ab0040e9ae5b
DylanHo <hhb_1997@outlook.com>
2018-04-11 23:53:11 +0800

    Reducers updated

--- ReduxStarter/src/reducers/reducer_active_book.js
+++ ReduxStarter/src/reducers/reducer_active_book.js
@@ -1,4 +1,8 @@
 // state argument is not an application state, only the state the reducer is responsible for.
-export default function(state,action) {
-    
+export default function(state = null,action) {
+    switch(action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+    return state;
 }
