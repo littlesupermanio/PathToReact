87919cd60c47054b996d99a1af8ac210a5826163
DylanHo <hhb_1997@outlook.com>
2018-04-18 00:00:13 +0800

    WeatherList

---
+++ weather/src/containers/weather_list.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Weatherlist extends Component {
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return { weather }; // { weather } === { weather: weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default function(state =  null,action) {
-    console.log('Action received',action)
-    return state
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [],action) {
+    switch(action.type) {
+        case FETCH_WEATHER:
+            // return state.concat([action.payload.data]);
+            return [ action.payload.data, ...state ]; // [city,city,city] ES6 return an entirely new array instead of manipulating it.
+
+    }
 } 
