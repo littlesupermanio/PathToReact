9047760016c2edaf89dbd15fc57adfd7444002a6
DylanHo <hhb_1997@outlook.com>
2018-04-19 19:48:45 +0800

    Added Google Maps

--- weather/src/containers/weather_list.js
+++ weather/src/containers/weather_list.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        //const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273); 转换成摄氏度
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon,lat } = cityData.city.coord;
+
+        return (
+            <tr key={name}>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td>
+                    <Chart data={temps} color="orange"  units="K"/>
+                </td>
+                <td>
+                    <Chart data={pressures} color="green" units="hPa" />
+                </td>
+                <td>
+                    <Chart data={humidities} color="black" units="%"/>
+                </td>
+            </tr>
+        )
+    }
     render() {
         return (
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                    </tr>
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -4,7 +4,7 @@ export default function(state = [],action) {
     switch(action.type) {
         case FETCH_WEATHER:
             // return state.concat([action.payload.data]);
-            return [ action.payload.data, ...state ]; // [city,city,city] ES6 return an entirely new array instead of manipulating it.
+            return !action.payload.data?state:[ action.payload.data, ...state ]; // [city,city,city] ES6 return an entirely new array instead of manipulating it.
     }
     return state;
 } 
