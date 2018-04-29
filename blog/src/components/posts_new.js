1ef5cb9f6290de17ed103238968fe6c6b5fc9e2d
DylanHo <hhb_1997@outlook.com>
2018-04-29 23:35:11 +0800

    Added react-router and react-form validation

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -1,47 +1,54 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-control ${touched && error ? 'is-invalid' : ''}`
         return (
             <div className="form-group">
                 <label>{field.label}</label>
-                <input  
-                    className="form-control"
+                <input
+                    className={className}
                     /* onChange={field.input.onChange}
                     onBlur={field.input.onBlur} */
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                <div className="invalid-feedback">{touched ? error : ''}</div>
             </div>
         )
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values);
     }
 
     render() {
         const { handleSubmit } = this.props;
         return (
             <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <Field 
+                <Field
                     name="title"
-                    label="Title For Post" 
+                    label="Title For Post"
                     component={this.renderField}
                 />
-                <Field 
+                <Field
                     name="categories"
                     label="Categories"
                     component={this.renderField}
                 />
-                <Field 
+                <Field
                     name="content"
                     label="Post Content"
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -54,13 +61,13 @@ function validate(values) {
     // if(values.title.length < 3) {
     //     errors.title = 'Title must be at least 3 characters.'
     // }
-    if(!values.title || values.title.length < 3) {
+    if (!values.title || values.title.length < 3) {
         errors.title = "Enter a title";
     }
-    if(!values.categories) {
+    if (!values.categories) {
         errors.categories = "Enter some categories!";
     }
-    if(!values.content) {
+    if (!values.content) {
         errors.content = "Enter some content please!";
     }
 
@@ -72,7 +79,9 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
 
 
 // PostEdit.js
